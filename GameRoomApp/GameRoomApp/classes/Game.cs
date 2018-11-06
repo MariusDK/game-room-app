baf81fc2d21a8e3e98568eca631df41eab04eb37
MariusDK <mariusdk7@gmail.com>
2018-11-06 13:54:57 +0200

    Create first basic model

---
+++ GameRoomApp/GameRoomApp/classes/Game.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameRoomApp.classes
+{
+    [BsonIgnoreExtraElements]
+    public class Game
+    {
+        [BsonId]
+        public int Id { get; set;}
+        public string Name { get; set; }
+        public User User { get; set; }
+        public int Score { get; set; }
+        public DateTime PlayedOn { get; set; }
+        
+    }
+}
